86ce9f23ec9a528a8fc49f12e02da9d64c51f959
Aleksei Slesarev <aleksei@Mac-mini-Aleksei.local>
2018-08-07 18:33:32 +0700

    some changes

--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic.edit import FormView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
-# from django.http import HttpResponseRedirect
 from django.views.generic.base import View
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
