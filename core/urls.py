88494644432ba3ce92d7c8f3358c4fd58e42828a
Aleksei Slesarev <aleksei@Mac-mini-Aleksei.local>
2018-08-07 15:55:33 +0700

    add registration in progress

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexPageView.as_view(), name='index'),
+    path('signup/', views.RegisterFormView.as_view(), name='signup'),
+    path('login/', views.LoginFormView.as_view(), name='login'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexPageView(TemplateView):
+    template_name = 'index.html'
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+    success_url = 'login'
+    template_name = 'signup.html'
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+    success_url = ''
+
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect('')
