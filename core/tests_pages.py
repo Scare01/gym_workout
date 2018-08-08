c39d7ba7032c535c3f589f897e408cbf5af07f53
Aleksei Slesarev <aleksei@Mac-mini-Aleksei.local>
2018-08-08 09:34:09 +0700

    move tests for pages to own file add tests for login page

---
+++ core/tests_pages.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexPage(TestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_index_page_contains_correct_html(self):
+        response = self.client.get(reverse('index'))
+        self.assertContains(response,
+                            '<p>Welcom to gym wourkout app</p>')
+
+    def test_index_page_does_not_contains_incorrect_html(self):
+        response = self.client.get(reverse('index'))
+        self.assertNotContains(response, 'This is not correct html')
+
+
+class LoginPage(TestCase):
+
+    def test_status_code(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('login'))
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_contains_correct_html(self):
+        response = self.client.get(reverse('login'))
+        self.assertContains(response, '<h2>Log in</h2>')
+
+    def test_does_not_contains_incorrect_html(self):
+        response = self.client.get(reverse('login'))
+        self.assertNotContains(response, 'This is not a login page')
