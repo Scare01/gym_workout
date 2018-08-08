51c41c4c6fb3fe38961fe968c84dc7097dcafb90
Aleksei Slesarev <lexsl84@gmail.com>
2018-08-08 14:41:25 +0700

    add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Program(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    continuity = models.PositiveIntegerField(default=0)
+    source = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Excercises(models.Model):
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    weight = models.PositiveIntegerField(default=0)
+    rounds_count = models.PositiveIntegerField(default=0)
+    max_rep = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Rounds(models.Model):
+    excercise = models.ForeignKey(Excercises, on_delete=models.CASCADE)
+    current_rep = models.PositiveIntegerField(default=0)
