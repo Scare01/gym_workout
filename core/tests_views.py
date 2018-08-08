583173450b638297dad2a03dc35a85d8b119d033
Aleksei Slesarev <aleksei@Mac-mini-Aleksei.local>
2018-08-08 09:15:00 +0700

    add tests for index page

--- core/tests_views.py
+++ core/tests_views.py
@@ -1,8 +1,29 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
-class LogInTest(TestCase):
+class IndexPage(TestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_index_page_contains_correct_html(self):
+        response = self.client.get(reverse('index'))
+        self.assertContains(response,
+                            '<p>Welcom to gym wourkout app</p>')
+
+
+class LoginUser(TestCase):
     def setUp(self):
         self.credentials = {
             'username': 'testuser',
